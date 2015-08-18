0e6c902293156ccf7412985641cf55e59c17279d
Jedzkie <jedzkie13@rocketmail.com>
2015-08-18 21:54:06 +0800

    Initial commit.
    Signed-off-by: Jedzkie <jedzkie13@rocketmail.com>

---
+++ UserKeys_s.lua
@@ -0,0 +1,15 @@
+USERKEY_1 = {
+	[9] = { EXE = "Hotkey 2-1", KEY1 = 81 },
+	[10] = { EXE = "Hotkey 2-2", KEY1 = 87 },
+	[11] = { EXE = "Hotkey 2-3", KEY1 = 69 },
+	[12] = { EXE = "Hotkey 2-4", KEY1 = 82 },
+	[13] = { EXE = "Hotkey 2-5", KEY1 = 84 },
+	[14] = { EXE = "Hotkey 2-6", KEY1 = 89 },
+	[18] = { EXE = "Hotkey 3-1", KEY1 = 65 },
+	[19] = { EXE = "Hotkey 3-2", KEY1 = 83 },
+	[20] = { EXE = "Hotkey 3-3", KEY1 = 68 },
+	[21] = { EXE = "Hotkey 3-4", KEY1 = 70 },
+	[22] = { EXE = "Hotkey 3-5", KEY1 = 71 },
+	[23] = { EXE = "Hotkey 3-6", KEY1 = 72 },
+}
+
